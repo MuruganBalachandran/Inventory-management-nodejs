6f9e2f89310ad1e5ed432d5a068cb15cad5c7b02
MuruganBalachandran <10murugan01@gmail.com>
2026-01-19 15:30:47 +0530

    Resolved merge conflict in admin.js

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -1,24 +1,17 @@
 // region imports
 const mongoose = require("mongoose");
-<<<<<<< HEAD
 const chalk = require("chalk");
-=======
->>>>>>> 444f163a5ca72f883d3a71eaa4076d959c28b34b
+
 // endregion
 
 // region connect to db
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URL);
-<<<<<<< HEAD
     console.log(chalk.green("Connected to MongoDB"));
-  } catch (error) {
-    console.error(chalk.red("Error connecting to MongoDB:"), error);
-=======
-    console.log("Connected to MongoDB");
+  
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
->>>>>>> 444f163a5ca72f883d3a71eaa4076d959c28b34b
     process.exit(1);
   }
 };
@@ -26,8 +19,4 @@ const connectDB = async () => {
 
 // region function call
 connectDB();
-<<<<<<< HEAD
 // endregion
-=======
-// endregiomn
->>>>>>> 444f163a5ca72f883d3a71eaa4076d959c28b34b
